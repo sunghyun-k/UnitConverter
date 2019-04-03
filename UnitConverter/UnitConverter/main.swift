a36458353a0dc9105abc1ec90ed8b1e478a4ed00
Sunghyun Kim <sunghyun_k@icloud.com>
2019-04-03 12:16:07 +0900

    2-6 무게 변환

--- UnitConverter/UnitConverter/main.swift
+++ UnitConverter/UnitConverter/main.swift
@@ -1,15 +1,19 @@
 import Foundation
 
 // 숫자와 문자 나누기
-func divide(_ input: String) -> (number: Double, from: String, to: String)? {
+func formatter(_ input: String) -> (number: Double, from: String, to: String)? {
     
-    var from = ""
-    var to = ""
+    var from = String()
+    var to = String()
     
     let inputUnit = input.trimmingCharacters(in: CharacterSet(charactersIn: "0123456789."))
     
     let inputUnitSplited = inputUnit.split(separator: " ")
-    from = String(inputUnitSplited[0])
+    if inputUnitSplited.indices.contains(0) {
+        from = String(inputUnitSplited[0])
+    } else {
+        return nil
+    }
     if inputUnitSplited.indices.contains(1) {
         to = String(inputUnitSplited[1])
     }
@@ -17,6 +21,7 @@ func divide(_ input: String) -> (number: Double, from: String, to: String)? {
     guard let number = Double(input.trimmingCharacters(in: CharacterSet(charactersIn: "0123456789.").inverted)) else {
         return nil
     }
+    print("number: \(number), from: \(from), to: \(to)")
     return (number, from, to)
 }
 
@@ -25,64 +30,111 @@ func divide(_ input: String) -> (number: Double, from: String, to: String)? {
 let meterToInchMultiplier = 39.37
 let inchToCentimeterMuliplier = 2.54
 let yardToInchMultiplier: Double = 36
+let kilogramToGramMultiplier: Double = 1000
+let poundToGramMultiplier = 453.592
+let ounceToGramMultiplier = 28.35
 
-// to inch 변환 함수들
-func meterToInch(_ input: Double) -> Double {
-    return input * meterToInchMultiplier
-}
-func centimeterToInch(_ input: Double) -> Double {
-    return input / inchToCentimeterMuliplier
-}
-func yardToInch(_ input: Double) -> Double {
-    return input * yardToInchMultiplier
-}
-
-// from inch 변환 함수들
-func inchToMeter(_ input: Double) -> Double {
-    return input / meterToInchMultiplier
-}
-func inchToCentimeter(_ input: Double) -> Double {
-    return input * inchToCentimeterMuliplier
-}
-func inchToYard(_ input: Double) -> Double {
-    return input / yardToInchMultiplier
+// to inch 변환 함수
+func toInch(_ input: Double, from inputUnit: String) -> Double? {
+    var result = Double()
+    switch inputUnit {
+    case "m":
+        result = input * meterToInchMultiplier
+    case "cm":
+        result = input / inchToCentimeterMuliplier
+    case "yard":
+        result = input * yardToInchMultiplier
+    case "inch":
+        result = input
+    default:
+        return nil
+    }
+    return result
 }
 
-// 변환하기
+// to gram 변환 함수
+func toGram(_ input: Double, from inputUnit: String) -> Double? {
+    var result = Double()
+    switch inputUnit {
+    case "kg":
+        result = input * kilogramToGramMultiplier
+    case "lb", "lbs":
+        result = input * poundToGramMultiplier
+    case "oz":
+        result = input * ounceToGramMultiplier
+    case "g":
+        result = input
+    default:
+        return nil
+    }
+    return result
+}
+
+// from inch 변환 함수
+func fromInch(_ input: Double, to outputUnit: String) -> String {
+    var resultNumber = Double()
+    var unit = String()
+    switch outputUnit {
+    case "m":
+        resultNumber = input / meterToInchMultiplier
+        unit = "m"
+    case "cm":
+        resultNumber = input * inchToCentimeterMuliplier
+        unit = "cm"
+    case "yard":
+        resultNumber = input / yardToInchMultiplier
+        unit = "yard"
+    case "inch", "":
+        resultNumber = input
+        unit = "inch"
+    default:
+        return "지원하지 않는 범위입니다."
+    }
+    return String(resultNumber) + unit
+}
+
+// from gram 변환 함수
+func fromGram(_ input: Double, to outputUnit: String) -> String {
+    var resultNumber = Double()
+    var unit = String()
+    switch outputUnit {
+    case "kg":
+        resultNumber = input / kilogramToGramMultiplier
+        unit = "kg"
+    case "lbs", "lb":
+        resultNumber = input / poundToGramMultiplier
+        unit = "lbs"
+    case "oz":
+        resultNumber = input / ounceToGramMultiplier
+        unit = "oz"
+    case "g", "":
+        resultNumber = input
+        unit = "g"
+    default:
+        return "지원하지 않는 범위입니다."
+    }
+    return String(resultNumber) + unit
+}
+
+
+
+
+// 변환기
 func convertUnit(_ input: String) -> String {
-    guard let dividedNumber = divide(input) else {
+    
+    guard let formatedInfo = formatter(input) else {
         return "올바른 형식이 아닙니다."
     }
-    var result = Double()
+    var result = ""
     
-    switch dividedNumber.from {
-    case "m":
-        result = meterToInch(dividedNumber.number)
-    case "cm":
-        result = centimeterToInch(dividedNumber.number)
-    case "yard":
-        result = yardToInch(dividedNumber.number)
-    case "inch":
-        result = dividedNumber.number
-    default:
+    if let number = toInch(formatedInfo.number, from: formatedInfo.from) {
+        result = fromInch(number, to: formatedInfo.to)
+    } else if let number = toGram(formatedInfo.number, from: formatedInfo.from) {
+        result = fromGram(number, to: formatedInfo.to)
+    } else {
         return "지원하지 않는 범위입니다."
     }
-    
-    switch dividedNumber.to {
-    case "m":
-        result = inchToMeter(result)
-        return String(result) + "m"
-    case "cm":
-        result = inchToCentimeter(result)
-        return String(result) + "cm"
-    case "yard":
-        result = inchToYard(result)
-        return String(result) + "yard"
-    case "inch", "":
-        return String(result) + "inch"
-    default:
-        return "지원하지 않는 범위입니다."
-    }
+    return result
 }
 
 // 유저 컨트롤
